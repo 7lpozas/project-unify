492b7fefcf273c58e5cede07054c67363e607d9c
noutyourlasagna <matiasvasquezplass@gmail.com>
2024-10-21 22:38:40 -0300

    REQ9

--- src/pages/Calculator.jsx
+++ src/pages/Calculator.jsx
@@ -16,6 +16,7 @@ function Calculator() {
   const [universidades, setUniversidades] = useState([]);
   const [carreras, setCarreras] = useState([]);
   const [filteredPrograms, setFilteredPrograms] = useState([]);
+  const [sortOrder, setSortOrder] = useState('asc');
 
   useEffect(() => {
     // Llamadas a los endpoints para obtener toda la información
@@ -116,142 +117,162 @@ function Calculator() {
     setFilteredPrograms(filtered); // Guarda los resultados filtrados
   };
   
+  const handleSort = () => {
+    const sortedPrograms = [...filteredPrograms].sort((a, b) => {
+      return sortOrder === 'asc'
+        ? a.puntajeCorte - b.puntajeCorte
+        : b.puntajeCorte - a.puntajeCorte;
+    });
+    setFilteredPrograms(sortedPrograms);
+    setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc'); // Alterna entre ascendente y descendente
+  };
+
+
   return (
     <div className="container mx-auto px-4 py-8">
       <h2 className="text-3xl font-bold mb-6 text-center">Encuentra las universidades que mejor se adaptan a ti</h2>
       <p className="text-lg text-gray-600 mb-8 text-center">Ingresa tus puntajes PAES y te ayudaremos a encontrar las mejores opciones para tu futuro académico.</p>
       
       <form onSubmit={handleSubmit} className="max-w-2xl mx-auto bg-white shadow-md rounded-lg p-6">
-      <div className="mb-8">
-  <h3 className="text-xl font-semibold mb-4">Campos obligatorios</h3>
-  <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-    <div>
-      <input
-        type="number"
-        name="matematicas1"
-        value={scores.matematicas1}
-        onChange={handleInputChange}
-        placeholder="Puntaje Matemáticas 1 (M1)"
-        required
-        className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary"
-      />
-      {errors.matematicas1 && <p className="text-red-500">{errors.matematicas1}</p>}
-    </div>
+        <div className="mb-8">
+          <h3 className="text-xl font-semibold mb-4">Campos obligatorios</h3>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            {/* Input fields */}
+            <div>
+              <input
+                type="number"
+                name="matematicas1"
+                value={scores.matematicas1}
+                onChange={handleInputChange}
+                placeholder="Puntaje Matemáticas 1 (M1)"
+                required
+                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary"
+              />
+              {errors.matematicas1 && <p className="text-red-500">{errors.matematicas1}</p>}
+            </div>
+            <div>
+              <input
+                type="number"
+                name="lectura"
+                value={scores.lectura}
+                onChange={handleInputChange}
+                placeholder="Puntaje C. Lectora"
+                required
+                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary"
+              />
+              {errors.lectura && <p className="text-red-500">{errors.lectura}</p>}
+            </div>
+            <div>
+              <input
+                type="number"
+                name="ranking"
+                value={scores.ranking}
+                onChange={handleInputChange}
+                placeholder="Puntaje Ranking"
+                required
+                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary"
+              />
+              {errors.ranking && <p className="text-red-500">{errors.ranking}</p>}
+            </div>
+            <div>
+              <input
+                type="number"
+                name="nem"
+                value={scores.nem}
+                onChange={handleInputChange}
+                placeholder="Puntaje NEM"
+                required
+                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary"
+              />
+              {errors.nem && <p className="text-red-500">{errors.nem}</p>}
+            </div>
+          </div>
+        </div>
 
-    <div>
-      <input
-        type="number"
-        name="lectura"
-        value={scores.lectura}
-        onChange={handleInputChange}
-        placeholder="Puntaje C. Lectora"
-        required
-        className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary"
-      />
-      {errors.lectura && <p className="text-red-500">{errors.lectura}</p>}
-    </div>
-
-    <div>
-      <input
-        type="number"
-        name="ranking"
-        value={scores.ranking}
-        onChange={handleInputChange}
-        placeholder="Puntaje Ranking"
-        required
-        className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary"
-      />
-      {errors.ranking && <p className="text-red-500">{errors.ranking}</p>}
-    </div>
-
-    <div>
-      <input
-        type="number"
-        name="nem"
-        value={scores.nem}
-        onChange={handleInputChange}
-        placeholder="Puntaje NEM"
-        required
-        className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary"
-      />
-      {errors.nem && <p className="text-red-500">{errors.nem}</p>}
-    </div>
-  </div>
-</div>
-
-<div className="mb-8">
-  <h3 className="text-xl font-semibold mb-4">Campos opcionales</h3>
-  <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-    <div>
-      <input
-        type="number"
-        name="matematicas2"
-        value={scores.matematicas2}
-        onChange={handleInputChange}
-        placeholder="Puntaje Matemáticas 2 (M2)"
-        className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary"
-      />
-      {errors.matematicas2 && <p className="text-red-500">{errors.matematicas2}</p>}
-    </div>
-    <div>
-      <input
-        type="number"
-        name="historia"
-        value={scores.historia}
-        onChange={handleInputChange}
-        placeholder="Puntaje Historia"
-        className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary"
-      />
-      {errors.historia && <p className="text-red-500">{errors.historia}</p>}
-    </div>
-    <div>
-      <input
-        type="number"
-        name="ciencias"
-        value={scores.ciencias}
-        onChange={handleInputChange}
-        placeholder="Puntaje Ciencias"
-        className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary"
-      />
-      {errors.ciencias && <p className="text-red-500">{errors.ciencias}</p>}
-    </div>
-  </div>
-</div>
-
+        <div className="mb-8">
+          <h3 className="text-xl font-semibold mb-4">Campos opcionales</h3>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            {/* Optional input fields */}
+            <div>
+              <input
+                type="number"
+                name="matematicas2"
+                value={scores.matematicas2}
+                onChange={handleInputChange}
+                placeholder="Puntaje Matemáticas 2 (M2)"
+                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary"
+              />
+              {errors.matematicas2 && <p className="text-red-500">{errors.matematicas2}</p>}
+            </div>
+            <div>
+              <input
+                type="number"
+                name="historia"
+                value={scores.historia}
+                onChange={handleInputChange}
+                placeholder="Puntaje Historia"
+                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary"
+              />
+              {errors.historia && <p className="text-red-500">{errors.historia}</p>}
+            </div>
+            <div>
+              <input
+                type="number"
+                name="ciencias"
+                value={scores.ciencias}
+                onChange={handleInputChange}
+                placeholder="Puntaje Ciencias"
+                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary"
+              />
+              {errors.ciencias && <p className="text-red-500">{errors.ciencias}</p>}
+            </div>
+          </div>
+        </div>
 
         <button type="submit" className="w-full btn btn-primary">Calcular</button>
       </form>
 
-      <div className="mt-12 overflow-x-auto">
-        <table className="w-full bg-white shadow-md rounded-lg overflow-hidden">
-          <thead className="bg-gray-100">
-            <tr>
-              <th className="px-4 py-2 text-left">Carrera</th>
-              <th className="px-4 py-2 text-left">Universidad</th>
-              <th className="px-4 py-2 text-left">Puntaje corte</th>
-              <th className="px-4 py-2 text-left">Puntaje ponderado</th>
-            </tr>
-          </thead>
-          <tbody>
-  {filteredPrograms.length > 0 ? (
-    filteredPrograms.map((pr) => (
-      <tr key={`${pr.idCarrera}-${pr.idUniversidad}`}>
-        <td className="px-4 py-2">{getNombreCarrera(pr.idCarrera)}</td>
-        <td className="px-4 py-2">{getNombreUniversidad(pr.idUniversidad)}</td>
-        <td className="px-4 py-2">{pr.puntajeCorte}</td>
-        <td className="px-4 py-2">
-          {pr.ponderacionUsuario.toFixed(2)} {/* Mostramos el valor calculado */}
-        </td>
-      </tr>
-    ))
-  ) : (
-    <tr>
-      <td colSpan="4" className="text-center px-4 py-2">No hay resultados que superen el puntaje de corte</td>
-    </tr>
-  )}
-</tbody>
+      <div className="mt-12">
+        <button onClick={handleSort} className="btn btn-secondary mb-4">
+          Ordenar por puntaje de corte ({sortOrder === 'asc' ? 'Ascendente' : 'Descendente'})
+        </button>
 
-        </table>
+        <div className="overflow-x-auto">
+          <table className="w-full bg-white shadow-md rounded-lg overflow-hidden">
+            <thead className="bg-gray-100">
+              <tr>
+                <th className="px-4 py-2 text-left">Carrera</th>
+                <th className="px-4 py-2 text-left">Universidad</th>
+                <th className="px-4 py-2 text-left">Puntaje corte</th>
+                <th className="px-4 py-2 text-left">Puntaje ponderado</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredPrograms.length > 0 ? (
+                filteredPrograms.map((pr) => (
+                  <tr key={`${pr.idCarrera}-${pr.idUniversidad}`}>
+                    <td className="px-4 py-2">{getNombreCarrera(pr.idCarrera)}</td>
+                    <td className="px-4 py-2">{getNombreUniversidad(pr.idUniversidad)}</td>
+                    <td className="px-4 py-2">{pr.puntajeCorte}</td>
+                    <td className="px-4 py-2">
+                      {((scores.matematicas1 * pr.ponderacionM1) +
+                      (scores.lectura * pr.ponderacionLeguaje) +
+                      (scores.ranking * pr.ponderacionRanking) +
+                      (scores.nem * pr.ponderacionNem) +
+                      (scores.matematicas2 * pr.ponderacionM2) +
+                      (scores.historia * pr.ponderacionHistoria) +
+                      (scores.ciencias * pr.ponderacionCiencias)) / 100}
+                    </td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td colSpan="4" className="text-center px-4 py-2">No hay resultados que superen el puntaje de corte</td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );

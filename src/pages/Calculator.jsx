7a968effefad36e5daa1225cb11a5a983a5c177e
noutyourlasagna <matiasvasquezplass@gmail.com>
2024-10-21 22:22:43 -0300

    REQ4

--- src/pages/Calculator.jsx
+++ src/pages/Calculator.jsx
@@ -10,6 +10,8 @@ function Calculator() {
     historia: '',
     ciencias: ''
   });
+  const [errors, setErrors] = useState({});
+
   const [programs, setPrograms] = useState([]); // Para guardar toda la información del backend
   const [universidades, setUniversidades] = useState([]); // Para guardar las universidades
   const [carreras, setCarreras] = useState([]); // Para guardar las carreras
@@ -52,14 +54,37 @@ function Calculator() {
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
+    const numValue = parseInt(value, 10);
+    
+    // Solo validar si el campo no está vacío
+    if (value !== '' && (numValue < 100 || numValue > 1000)) {
+      setErrors(prevErrors => ({
+        ...prevErrors,
+        [name]: 'El puntaje debe estar entre 100 y 1000 puntos.'
+      }));
+    } else {
+      setErrors(prevErrors => ({
+        ...prevErrors,
+        [name]: ''
+      }));
+    }
+  
     setScores(prevScores => ({
       ...prevScores,
       [name]: value
     }));
   };
+  
+  
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    const hasErrors = Object.values(errors).some(error => error !== '');
+  if (hasErrors) {
+    alert('Por favor, corrige los errores antes de continuar.');
+    return;
+  }
 
     const filtered = programs.filter(pr => {
       const ponderacionUsuario =
@@ -85,24 +110,102 @@ function Calculator() {
       <p className="text-lg text-gray-600 mb-8 text-center">Ingresa tus puntajes PAES y te ayudaremos a encontrar las mejores opciones para tu futuro académico.</p>
       
       <form onSubmit={handleSubmit} className="max-w-2xl mx-auto bg-white shadow-md rounded-lg p-6">
-        <div className="mb-8">
-          <h3 className="text-xl font-semibold mb-4">Campos obligatorios</h3>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-            <input type="number" name="matematicas1" value={scores.matematicas1} onChange={handleInputChange} placeholder="Puntaje Matemáticas 1 (M1)" required className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary" />
-            <input type="number" name="lectura" value={scores.lectura} onChange={handleInputChange} placeholder="Puntaje C. Lectora" required className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary" />
-            <input type="number" name="ranking" value={scores.ranking} onChange={handleInputChange} placeholder="Puntaje Ranking" required className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary" />
-            <input type="number" name="nem" value={scores.nem} onChange={handleInputChange} placeholder="Puntaje NEM" required className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary" />
-          </div>
-        </div>
+      <div className="mb-8">
+  <h3 className="text-xl font-semibold mb-4">Campos obligatorios</h3>
+  <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+    <div>
+      <input
+        type="number"
+        name="matematicas1"
+        value={scores.matematicas1}
+        onChange={handleInputChange}
+        placeholder="Puntaje Matemáticas 1 (M1)"
+        required
+        className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary"
+      />
+      {errors.matematicas1 && <p className="text-red-500">{errors.matematicas1}</p>}
+    </div>
 
-        <div className="mb-8">
-          <h3 className="text-xl font-semibold mb-4">Campos opcionales</h3>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-            <input type="number" name="matematicas2" value={scores.matematicas2} onChange={handleInputChange} placeholder="Puntaje Matemáticas 2 (M2)" className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary" />
-            <input type="number" name="historia" value={scores.historia} onChange={handleInputChange} placeholder="Puntaje Historia" className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary" />
-            <input type="number" name="ciencias" value={scores.ciencias} onChange={handleInputChange} placeholder="Puntaje Ciencias" className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary" />
-          </div>
-        </div>
+    <div>
+      <input
+        type="number"
+        name="lectura"
+        value={scores.lectura}
+        onChange={handleInputChange}
+        placeholder="Puntaje C. Lectora"
+        required
+        className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary"
+      />
+      {errors.lectura && <p className="text-red-500">{errors.lectura}</p>}
+    </div>
+
+    <div>
+      <input
+        type="number"
+        name="ranking"
+        value={scores.ranking}
+        onChange={handleInputChange}
+        placeholder="Puntaje Ranking"
+        required
+        className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary"
+      />
+      {errors.ranking && <p className="text-red-500">{errors.ranking}</p>}
+    </div>
+
+    <div>
+      <input
+        type="number"
+        name="nem"
+        value={scores.nem}
+        onChange={handleInputChange}
+        placeholder="Puntaje NEM"
+        required
+        className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary"
+      />
+      {errors.nem && <p className="text-red-500">{errors.nem}</p>}
+    </div>
+  </div>
+</div>
+
+<div className="mb-8">
+  <h3 className="text-xl font-semibold mb-4">Campos opcionales</h3>
+  <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+    <div>
+      <input
+        type="number"
+        name="matematicas2"
+        value={scores.matematicas2}
+        onChange={handleInputChange}
+        placeholder="Puntaje Matemáticas 2 (M2)"
+        className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary"
+      />
+      {errors.matematicas2 && <p className="text-red-500">{errors.matematicas2}</p>}
+    </div>
+    <div>
+      <input
+        type="number"
+        name="historia"
+        value={scores.historia}
+        onChange={handleInputChange}
+        placeholder="Puntaje Historia"
+        className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary"
+      />
+      {errors.historia && <p className="text-red-500">{errors.historia}</p>}
+    </div>
+    <div>
+      <input
+        type="number"
+        name="ciencias"
+        value={scores.ciencias}
+        onChange={handleInputChange}
+        placeholder="Puntaje Ciencias"
+        className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary"
+      />
+      {errors.ciencias && <p className="text-red-500">{errors.ciencias}</p>}
+    </div>
+  </div>
+</div>
+
 
         <button type="submit" className="w-full btn btn-primary">Calcular</button>
       </form>
